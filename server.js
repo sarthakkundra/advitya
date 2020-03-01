3c34c3bb0852ee403e9e8c1fa2f5fabfb7a847df
sarthakkundra <sarthakkundra21@gmail.com>
2020-03-01 15:40:06 +0530

    linked the login wireframe

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ var app = express();
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) =>{
-    res.sendFile('./public/index.html')
+    res.sendFile(__dirname + '/public/index.html');
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/form.html');
+})
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('Server started on ${PORT}'));
