8e3a6be5339fa83b31e08d55d1a2bd9e192d8fda
sarthakkundra <sarthakkundra21@gmail.com>
2020-03-02 13:15:24 +0530

    login linkage error in auth-routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const authRoutes = require('./routes/auth-routes')
 
 var app = express();
 
@@ -9,9 +10,7 @@ app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/public/index.html');
 })
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/public/form.html');
-})
+app.use('/auth', authRoutes);
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('Server started on ${PORT}'));
