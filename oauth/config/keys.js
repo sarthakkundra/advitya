69bc36b0e7a3fa592f9c904a792a5616bc965a77
Ishaan <ishaanbansal19@gmail.com>
2020-03-04 19:52:54 +0530

    Initial commit

---
+++ oauth/config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+        clientID: '423402557336-s0djll5pggpclu222ipo1v0tohogr09e.apps.googleusercontent.com',
+        clientSecret: 'YXRfLs1xwstkRYOJKrzw-1I3'
+        },
+        mongodb: {
+            dbURI: 'mongodb+srv://ishaan:vitbhopal@advitya-jlp6r.mongodb.net/test?retryWrites=true&w=majority'
+        },
+        session: {
+            cookieKey: 'advitya'
+        }
+};
